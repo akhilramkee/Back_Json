f68b5da96f9f2739bb3ad78be9600f442aec374a
Akhilesh <akhilramkee123@gmail.com>
2020-03-21 21:19:28 +0530

    Debug

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(session({
 
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header("Access-Control-Allow-Header","Origin,X-Requested-With,Content-Type,Accept");
     res.header("Access-Control-Allow-Credentials",true);
     next();
